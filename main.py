4d44fead812c78486870be4a40be6c0afc8bbe59
RayGre90 <reinhardtgrethe@gmail.com>
2025-05-05 17:08:56 +0200

    FPS added. All working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 
+pygame.init()
+
 print("Starting Asteroids!")
 print(f"Screen Width: {SCREEN_WIDTH}")
 print(f"Screen Height: {SCREEN_HEIGHT}")
@@ -11,12 +13,16 @@ fill = (0, 0, 0)
 screen.fill(fill)
 pygame.display.flip()
 
+pygame.time.delay(1000)
+dt = 0
+clock = pygame.time.Clock()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-            
+
 def main():
     if __name__ == "__main__":
         main()
