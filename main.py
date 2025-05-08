de7be2f49e5fd4bf75b88593d68bacb4798b9c17
RayGre90 <reinhardtgrethe@gmail.com>
2025-05-08 19:00:40 +0200

    Player on screen. No movement. All working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 
@@ -7,10 +8,14 @@ print("Starting Asteroids!")
 print(f"Screen Width: {SCREEN_WIDTH}")
 print(f"Screen Height: {SCREEN_HEIGHT}")
 
+player = Player(x = SCREEN_WIDTH / 2, 
+                y = SCREEN_HEIGHT / 2
+)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 fill = (0, 0, 0)
 screen.fill(fill)
+player.draw(screen)  
 pygame.display.flip()
 
 pygame.time.delay(1000)
