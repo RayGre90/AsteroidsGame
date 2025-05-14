210d224a6bba284b5909fa4eff62b5bbf96306f6
RayGre90 <reinhardtgrethe@gmail.com>
2025-05-14 18:25:16 +0200

    Busy building the asteroids. Keep updating

---
+++ Asteroid.py
@@ -0,0 +1,12 @@
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(
+            screen, 
+            "white", 
+            (int(self.position.x), int(self.position.y)),
+            self.radius,
+            width=2
+        )
